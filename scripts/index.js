df93ceb307589e6a8b1d01b9824bf39e52eae223
Albert Okon <albannytechnologies@gmail.com>
2025-10-14 17:02:48 +0100

    Add new SVG logos and update index.js with variable declarations
    - Added dlogo.svg, facebook-messenger.svg, site-alt.svg, and spotify.svg to the logos directory.
    - Implemented variable declarations and console logging in index.js for demonstration purposes.

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+const name = "example";
+console.log(`This is the ${name} package.`);
+
+
+// TO DECALRE A VARIABLE, USE THE 'const' KEYWORD.
+
+const booleanVariable = true;
+const numberVariable = 42;
+const stringVariable = "Hello, World!";
+const arrayVariable = [1, 2, 3, 4, 5];
+const objectVariable = { key: "value", anotherKey: 123 };
+
+
+objectVariable.key = "newValue";
+
+objectVariable.anotherKey = "newValue";
+
+console.log(booleanVariable);
+console.log(numberVariable);
+console.log(stringVariable);
+console.log(arrayVariable);
+console.log(objectVariable);
